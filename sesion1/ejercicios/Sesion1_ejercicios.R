0a82ad9b6fad3ad15dc53e4c59bc815d192d3904
pablocal <pablocal@usal.es>
2019-07-14 12:25:19 +0200

    sesion1 ready

---
+++ sesion1/ejercicios/Sesion1_ejercicios.R
@@ -0,0 +1,90 @@
+###############################################################
+####### Cómo sobrevivir a una pelea con R - Ejercicios ########
+###############################################################
+
+rm(list = ls())
+library(tidyverse)
+
+
+# A. Tipos de objetos -----------------------------------------------------
+
+## A.1
+
+
+
+## A.2
+
+
+
+## A.3
+
+
+
+## A.4
+
+
+
+
+# B. Indexar objetos ------------------------------------------------------
+
+## B.1
+
+ingresos <- c(Marcos = 1500, Marta = 200,
+              Juan = 2050, Isabel = 300)
+
+
+## B.2
+
+head(x = mtcars, n = 5)
+
+
+## B.3
+
+
+
+## B.4
+
+set.seed(123)
+sample(x = rownames(mtcars_small), size = 2) # esto no funciona si mtcars_small no se ha creado
+
+
+
+# C. Modificar vectores ----------------------------------------------------
+
+## C.1
+
+
+
+## C.2
+
+
+
+## C.3
+
+
+
+## C.4
+
+datos <- c("Juan Antonio, 28", "María Jesús, 29",
+           "Pedro Sarmiento, 21", "Josefa Maura, +99")
+
+
+
+# D. Transformar objetos --------------------------------------------------
+
+## D.1
+
+
+
+## D.2
+
+
+
+## D.3
+
+
+
+
+
+
+
